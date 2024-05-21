8455973ef1ab8d4a33bfe5a5876b20ca6e433ff5
deagle <170202513+deaglebullet@users.noreply.github.com>
2024-05-21 22:15:01 +0100

    Update version

--- win_wallpaper/main.py
+++ win_wallpaper/main.py
@@ -56,7 +56,7 @@ def modify_image(image_path: str, rgb_value: tuple[int]) -> None:
 def main() -> int:
     logging.basicConfig(format="[%(name)s] %(levelname)s: %(message)s", level=logging.INFO)
 
-    version = "0.4.0"
+    version = "1.0.0"
     images: set[str] = set()
 
     # for packed binary
